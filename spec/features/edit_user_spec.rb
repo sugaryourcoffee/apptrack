ad426e32ec06bd3a4c1c1842d88973a976eecfb8
Pierre <pierre@sugaryourcoffee.de>
2013-10-30 06:09:45 +0100

    Prevent to edit admin attribute from within the web application

--- spec/features/edit_user_spec.rb
+++ spec/features/edit_user_spec.rb
@@ -34,4 +34,19 @@ describe "User" do
     expect(current_path).to eq user_path(user)
   end
 
+  describe "don't allow to update admin attribute", type: :request do
+    let(:user) { User.create(user_attributes) }
+
+    let(:params) do
+      { user: { admin: true, password: user.password,
+                password_confirmation: user.password } }
+    end
+
+    before do
+      sign_in user, no_capybara: true
+      patch user_path(user), params
+    end
+    specify { expect(user.reload).not_to be_admin }
+  end
+
 end
